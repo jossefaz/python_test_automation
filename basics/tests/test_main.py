7d5c79309e7a4dd37cd992eccb451675403acbb7
jossefaz <josef.azoulay@emedgene.com>
2021-06-29 11:26:56 +0300

    add basic test example

---
+++ basics/tests/test_main.py
@@ -0,0 +1,14 @@
+from ..app.main import add
+
+def test_add_num():
+    assert add(1,2) == 3
+
+def test_add_str():
+    assert add("hello ", "world") == "hello world"
+
+class TestMain:
+    def test_add_num(self):
+        assert add(1, 2) == 3
+
+    def test_add_str(self):
+        assert add("hello ", "world") == "hello world"
